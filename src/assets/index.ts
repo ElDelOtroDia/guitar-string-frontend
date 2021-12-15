214566e04e273658f4e3134b5017e40e61cc9a4a
ElDelOtroDia <45863306+ElDelOtroDia@users.noreply.github.com>
2021-12-15 01:33:23 -0600

    login and register interfaces are done

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import { ReactComponent as GuitarPick } from "./svg/guitar-pick.svg";
+
+const images = { GuitarPick };
+
+export default images;
--- src/views/login/component.tsx
+++ src/views/login/component.tsx
@@ -1,9 +1,79 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import images from "../../assets";
+import "./styles.css";
 
 const Login: FC = () => {
-  return <div>
-    HOLA
-  </div>;
+  const { GuitarPick } = images;
+
+  const [isNewUser, setIsNewUser] = useState(false);
+
+  return (
+    <div className="login-container">
+      {!isNewUser ? (
+        <div className="form-container">
+          <div className="guitar-string-logo">
+            <GuitarPick className="guitar-pick" />
+            <div className="guitar-string">GuitarString</div>
+          </div>
+          <div className="form-input-container">
+            <label htmlFor="">Correo electrónico</label>
+            <input type="text" />
+          </div>
+          <div className="form-input-container">
+            <label htmlFor="">Contraseña</label>
+            <input type="password" />
+          </div>
+          <p>
+            ¿Nuevo usuario?{" "}
+            <span
+              className="register-here"
+              onClick={() => {
+                setIsNewUser(true);
+              }}
+            >
+              Regístrate aquí
+            </span>
+          </p>
+          <button className="login-button">Iniciar sesión</button>
+        </div>
+      ) : (
+        <div className="form-container">
+          <div className="guitar-string-logo">
+            <GuitarPick className="guitar-pick" />
+            <div className="guitar-string">GuitarString</div>
+          </div>
+          <div className="form-input-container">
+            <label htmlFor="">Nombre(s)</label>
+            <input type="text" />
+          </div>
+          <div className="form-input-container">
+            <label htmlFor="">Apellidos</label>
+            <input type="text" />
+          </div>
+          <div className="form-input-container">
+            <label htmlFor="">Correo electrónico</label>
+            <input type="text" />
+          </div>
+          <div className="form-input-container">
+            <label htmlFor="">Contraseña</label>
+            <input type="password" />
+          </div>
+          <p>
+            ¿Ya tienes una cuenta?{" "}
+            <span
+              className="register-here"
+              onClick={() => {
+                setIsNewUser(false);
+              }}
+            >
+              Inicia sesión
+            </span>
+          </p>
+          <button className="login-button">Registrarse</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Login;
